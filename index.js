6cd0b130c8281da3f48c2aa51a5e38bd1b3a5202
shreesantgithuber <105900616+shreesantgithuber@users.noreply.github.com>
2023-03-29 19:44:11 +0545

    added index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let typd=new Typed("#dekhau",{
+	strings: ["C","C#","Java","Python","HTML","CSS","Javascript"],
+	typeSpeed: 100,
+	backSpeed:100,
+	loop:true,
+	showCursor:false,
+	shuffle:true
+})
